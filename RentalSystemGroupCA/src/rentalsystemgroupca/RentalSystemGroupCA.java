510766a64e55c4da51ab8ebf1193ae470ef96c67
Franklin Arruda <franklin.arrudaa@gmail.com>
2023-12-05 05:39:04 +0000

    Initial commit
    Test commit only.
    
    Main class 'Rental System Group CA' created.

---
+++ RentalSystemGroupCA/src/rentalsystemgroupca/RentalSystemGroupCA.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rentalsystemgroupca;
+
+/**
+ *
+ * @author Franklin
+ */
+public class RentalSystemGroupCA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+             
+        // test commit message
+        messageTest();  
+    }
+    
+    static String msg = "This is a test commit message only!!!";
+    
+    static void messageTest(){
+        System.out.println(msg);
+    }
+}
